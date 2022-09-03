799f19642dd4c6bdc2030ce29e6e19fd91fdffb4
Addy Osmani <addyosmani@gmail.com>
2022-09-03 13:41:19 -0700

    Further pre-connect cleanup

--- components/MyHead/index.js
+++ components/MyHead/index.js
@@ -8,7 +8,7 @@ const MyHead = ({ children }) => (
     <link rel='preconnect' href='https://image.tmdb.org' />
     <link rel='preconnect' href='https://api.themoviedb.org' />
     {/* <link rel='preconnect' href='https://www.google-analytics.com' /> */}
-    <link rel='preconnect' href='https://content-autofill.googleapis.com' />
+    {/* <link rel='preconnect' href='https://content-autofill.googleapis.com' /> */}
     <link rel='preload' href={LOGO_IMAGE_PATH} as='image' media='(min-width: 80em)' />
     <link rel='preload' href={DARK_TMDB_IMAGE_PATH} as='image' media='(prefers-color-scheme: dark) and (min-width: 80em)' />
     <link rel='preload' href={LIGHT_TMDB_IMAGE_PATH} as='image' media='(prefers-color-scheme: light) and (min-width: 80em)' />
